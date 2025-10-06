b133e0e7171c152ab82ecc9b8c52e262cb3385a7
Thiago Cardozo <cardozothiago07@gmail.com>
2025-10-06 00:18:15 -0300

    Atualizando api

--- src/extracao_api.py
+++ src/extracao_api.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 import os
+import requests
+import json
 
 TIME_STAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
 
@@ -10,6 +12,12 @@ query = 'data science'
 tweet_fields = "tweet.fields=author_id,conversation_id,created_at,id,in_reply_to_user_id,public_metrics,lang,text"
 user_fields = "expansions=author_id&user.fields=id,name,username,created_at"
 
-url_raw = "https://api.x.com/2/tweets/search/recent?query={}&{}&{}{}{}".format(query, tweet_fields, user_fields, start_time, end_time)
+url_raw = f"https://labdados.com/2/tweets/search/recent?query={query}&{tweet_fields}&{user_fields}&start_time={start_time}&end_time={end_time}"
 
 bearer_token = os.environ.get("BEARER_TOKEN")
+headers = {"Authorization": "Bearer {}".format(bearer_token)}
+response = requests.request("GET", url_raw, headers=headers)
+
+json_response = response.json()
+
+print(json.dumps(json_response, indent=4, sort_keys=True))
